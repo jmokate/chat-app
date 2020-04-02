5742c9d69adcbccf14e007f1c8d9f248d489ca1c
jmokate <jmokate@gmail.com>
2020-04-02 16:48:58 -0500

    setting up API points

--- server.js
+++ server.js
@@ -1,13 +1,28 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use("/client", express.static("client"));
 
-app.get("/", (req, res) => {
+// GET messages
+app.get("/api/messages", (req, res) => {});
+
+//POST messages
+app.post("/api/messages", (req, res) => {});
+
+//GET all users
+app.get("/api/users", (req, res) => {});
+
+//POST a user (CREATE user)
+app.post("/api/users", (req, res) => {});
+
+//GET single user
+app.get("/api/users", (req, res) => {});
+
+app.get("/*", (req, res) => {
   res.send("hiya");
-  res.sendFile(path.join(__dirname + "/client/public/index.html"));
+  res.sendFile(path.join(__dirname, "client", "public", "index.html"));
 });
 
 app.listen(port, () => console.log(`app listening at ${port}`));
