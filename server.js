37baec2d56b4165f0f17e82ce218032780363519
jmokate <jmokate@gmail.com>
2020-04-02 14:46:52 -0500

    end wk 1

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const port = 5000;
 
+app.use("/client", express.static("client"));
+
+app.get("/", (req, res) => {
+  res.send("hiya");
+  res.sendFile(path.join(__dirname + "/client/public/index.html"));
+});
+
+app.listen(port, () => console.log(`app listening at ${port}`));
