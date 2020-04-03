502f9a37042ff8f84a3996a4e446bac8dd80fc8c
jmokate <jmokate@gmail.com>
2020-04-02 19:01:22 -0500

    created api folder

---
+++ api/Messages.js
@@ -0,0 +1,6 @@
+messages = [
+  { user: "You", message: "hi" },
+  { user: "Them", message: "yo what up" }
+];
+
+module.exports = messages;
--- server.js
+++ server.js
@@ -1,21 +1,29 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const messages = require("./api/Messages");
 const port = process.env.PORT || 5000;
 
 app.use(express.static(path.join(__dirname, "client/public")));
 
+//body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 // GET messages
 app.get("/api/messages", (req, res) => {
-  messages = [
-    { user: "You", message: "hi" },
-    { user: "Them", message: "yo what up" }
-  ];
   res.send(messages);
 });
 
 //POST messages
-app.post("/api/messages", (req, res) => {});
+app.post("/api/messages", (req, res) => {
+  const newMember = {
+    user: req.body.user,
+    message: req.body.message
+  };
+  messages.push(newMember);
+  res.send(messages);
+});
 
 //GET all users
 app.get("/api/users", (req, res) => {});
@@ -24,7 +32,7 @@ app.get("/api/users", (req, res) => {});
 app.post("/api/users", (req, res) => {});
 
 //GET single user
-app.get("/api/users/id", (req, res) => {});
+app.get("/api/users/:id", (req, res) => {});
 
 app.get("/*", (req, res) => {
   res.send("hiya");
