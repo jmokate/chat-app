5a9700e3e5c3fb435caff168ae5c6b48df4500bb
jmokate <jmokate@gmail.com>
2020-04-08 17:20:51 -0500

    working on queries

--- db.js
+++ db.js
@@ -14,8 +14,32 @@ connectToDb = async () => {
   return await client
     .connect()
     .then(() => console.log("connected to chat_app database"))
+    .then(() =>
+      client.query("INSERT INTO users VALUES($1, $2, $3)", [
+        null,
+        "dusty",
+        null
+      ])
+    )
+    .then(() => client.query("SELECT * FROM users WHERE id > $1", [3]))
+    .then(results => console.table(results.rows))
     .catch(err => console.error("connection error", err.stack))
     .finally(() => client.end());
 };
+
+//get all users from db
+queryUsers = async () => {};
+
+//create a user in db
+createUser = async () => {};
+
+// get all messages from db
+queryAllMessages = async () => {};
+
+// get a message by id in db
+queryMessageById = async () => {};
+
+//create a message in the db
+createMessage = async () => {};
 
 module.exports = { connectToDb };
