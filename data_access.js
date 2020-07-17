7847d1779615139ef55a5f71b0028c9d68521d48
jmokate <jmokate@gmail.com>
2020-07-17 17:19:15 -0500

    preparing code for deployment

--- data_access.js
+++ data_access.js
@@ -1,189 +1,195 @@
-const moment = require("moment");
 require("dotenv").config();
-const PASS = process.env.POSTGRES;
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
-
 //connecting to database
 const { Client } = require("pg");
-const client = new Client({
-  database: "chat_app",
-  user: "postgres",
-  port: 5432,
-  password: PASS
-});
+
+const localConfig = {
+	database: process.env.PG_DATABASE,
+	user: process.env.PG_USER,
+	port: process.env.PG_PORT,
+	password: process.env.PG_PASSWORD,
+};
+
+const herokuAddonConfig = {
+	connectionString: process.env.DATABASE_URL,
+};
+
+const client = new Client(
+	process.env.NODE_ENV === "production" ? herokuAddonConfig : localConfig
+);
 
 connectToDb = async () => {
-  // ASYNC CONNECT TO DB
-  try {
-    await client.connect();
-    console.log("connected to chat_app database");
-  } catch (err) {
-    console.log("Not connected to DB" + err);
-  }
+	// ASYNC CONNECT TO DB
+	try {
+		await client.connect();
+		console.log("connected to chat_app database");
+	} catch (err) {
+		console.log("Not connected to DB" + err);
+	}
 };
 
 queryInactiveUsers = async () => {
-  try {
-    const results = await client.query(
-      "SELECT * FROM users WHERE is_logged_in = false;"
-    );
-    return results.rows;
-  } catch (err) {
-    console.log(`Users not queried ${err}`);
-    return [];
-  }
+	try {
+		const results = await client.query(
+			"SELECT * FROM users WHERE is_logged_in = false;"
+		);
+		return results.rows;
+	} catch (err) {
+		console.log(`Users not queried ${err}`);
+		return [];
+	}
 };
 
 queryActiveUsers = async () => {
-  try {
-    await client.query("SELECT * FROM users WHERE is_logged_in = true;");
-    await client.query(
-      "UPDATE users SET is_logged_in = false WHERE last_active_at < NOW() - INTERVAL '20 minutes';"
-    );
-    const results = await client.query(
-      "SELECT * FROM users WHERE last_active_at > NOW() - INTERVAL '20 minutes' AND is_logged_in = true;"
-    );
-    return results.rows;
-  } catch (err) {
-    console.log(`Active users not queried ${err}`);
-    return [];
-  }
+	try {
+		await client.query("SELECT * FROM users WHERE is_logged_in = true;");
+		await client.query(
+			"UPDATE users SET is_logged_in = false WHERE last_active_at < NOW() - INTERVAL '20 minutes';"
+		);
+		const results = await client.query(
+			"SELECT * FROM users WHERE last_active_at > NOW() - INTERVAL '20 minutes' AND is_logged_in = true;"
+		);
+		return results.rows;
+	} catch (err) {
+		console.log(`Active users not queried ${err}`);
+		return [];
+	}
 };
 
 createUser = async (username, password) => {
-  try {
-    const hash = await bcrypt.hashSync(password, saltRounds);
-    await client.query("BEGIN");
-    const results = await client.query(
-      "INSERT INTO users(username, password) VALUES($1, $2) RETURNING id",
-      [username, hash]
-    );
+	try {
+		const hash = await bcrypt.hashSync(password, saltRounds);
+		await client.query("BEGIN");
+		const results = await client.query(
+			"INSERT INTO users(username, password) VALUES($1, $2) RETURNING id",
+			[username, hash]
+		);
 
-    await client.query("COMMIT");
-    return results.rows[0].id;
-  } catch (err) {
-    console.log(`User not created ${err}`);
-    await client.query("ROLLBACK");
-    return false;
-  }
+		await client.query("COMMIT");
+		return results.rows[0].id;
+	} catch (err) {
+		console.log(`User not created ${err}`);
+		await client.query("ROLLBACK");
+		return false;
+	}
 };
 
 queryAllMessages = async () => {
-  try {
-    const results = await client.query(
-      "SELECT m.user_id, m.id, m.text, m.created_date, u.username FROM messages AS m INNER JOIN users AS u ON u.id = m.user_id;"
-    );
-    return results.rows;
-  } catch (err) {
-    console.log(`Messages not queried ${err}`);
-    return [];
-  }
+	try {
+		const results = await client.query(
+			"SELECT m.user_id, m.id, m.text, m.created_date, u.username FROM messages AS m INNER JOIN users AS u ON u.id = m.user_id;"
+		);
+		return results.rows;
+	} catch (err) {
+		console.log(`Messages not queried ${err}`);
+		return [];
+	}
 };
 
 loginUser = async (name, password) => {
-  try {
-    await client.query("BEGIN");
-    const results = await client.query(
-      `SELECT * FROM users WHERE username = '${name}';`
-    );
-    const saltedPassword = results.rows[0].password;
-    const passwordMatch = await bcrypt.compare(password, saltedPassword);
+	try {
+		await client.query("BEGIN");
+		const results = await client.query(
+			`SELECT * FROM users WHERE username = '${name}';`
+		);
+		const saltedPassword = results.rows[0].password;
+		const passwordMatch = await bcrypt.compare(password, saltedPassword);
 
-    if (results.rows[0].is_logged_in == true) {
-      return {
-        isSuccessful: false,
-        errorMessage: "This user is already logged in"
-      };
-    }
+		if (results.rows[0].is_logged_in == true) {
+			return {
+				isSuccessful: false,
+				errorMessage: "This user is already logged in",
+			};
+		}
 
-    if (!passwordMatch) {
-      return {
-        isSuccessful: false,
-        errorMessage: "Incorrect password"
-      };
-    } else {
-      await client.query(
-        `UPDATE users SET last_active_at = NOW() WHERE username = '${name}'`
-      );
-      await client.query(
-        `UPDATE users SET is_logged_in = true WHERE username = '${name}'`
-      );
-      await client.query("COMMIT");
-    }
-    let successfulLogin = {
-      isSuccessful: true,
-      errorMessage: null,
-      user: results.rows[0]
-    };
-    return successfulLogin;
-  } catch (err) {
-    console.log(`User not logged in ${err}`);
-    return {
-      isSuccessful: false,
-      errorMessage: "Could not locate this username. Please register."
-    };
-  }
+		if (!passwordMatch) {
+			return {
+				isSuccessful: false,
+				errorMessage: "Incorrect password",
+			};
+		} else {
+			await client.query(
+				`UPDATE users SET last_active_at = NOW() WHERE username = '${name}'`
+			);
+			await client.query(
+				`UPDATE users SET is_logged_in = true WHERE username = '${name}'`
+			);
+			await client.query("COMMIT");
+		}
+		let successfulLogin = {
+			isSuccessful: true,
+			errorMessage: null,
+			user: results.rows[0],
+		};
+		return successfulLogin;
+	} catch (err) {
+		console.log(`User not logged in ${err}`);
+		return {
+			isSuccessful: false,
+			errorMessage: "Could not locate this username. Please register.",
+		};
+	}
 };
 
 logOutUser = async name => {
-  try {
-    await client.query("BEGIN");
-    const results = await client.query(
-      `SELECT * FROM users WHERE username = '${name}'`
-    );
-    await client.query(
-      `UPDATE users SET is_logged_in = false WHERE username = '${name}'`
-    );
-    await client.query("COMMIT");
-    return results.rows[0];
-  } catch (err) {
-    console.log(`Could not logout user ${err}`);
-    return false;
-  }
+	try {
+		await client.query("BEGIN");
+		const results = await client.query(
+			`SELECT * FROM users WHERE username = '${name}'`
+		);
+		await client.query(
+			`UPDATE users SET is_logged_in = false WHERE username = '${name}'`
+		);
+		await client.query("COMMIT");
+		return results.rows[0];
+	} catch (err) {
+		console.log(`Could not logout user ${err}`);
+		return false;
+	}
 };
 
 putLogoutUser = async id => {
-  try {
-    await client.query("BEGIN");
-    await client.query(`SELECT * FROM users WHERE id = '${id}'`);
-    await client.query(
-      `UPDATE users SET is_logged_in = false WHERE id = '${id}'`
-    );
-    await client.query("COMMIT");
-  } catch (err) {
-    console.log(`Could not logout user ${err}`);
-    return false;
-  }
+	try {
+		await client.query("BEGIN");
+		await client.query(`SELECT * FROM users WHERE id = '${id}'`);
+		await client.query(
+			`UPDATE users SET is_logged_in = false WHERE id = '${id}'`
+		);
+		await client.query("COMMIT");
+	} catch (err) {
+		console.log(`Could not logout user ${err}`);
+		return false;
+	}
 };
 
 //create a message in the db
 createMessage = async (userId, text, createdDate) => {
-  try {
-    await client.query("BEGIN");
-    const result = await client.query(
-      "INSERT INTO messages(user_id, text) VALUES($1, $2) RETURNING id, created_date, user_id, text",
-      [userId, text]
-    );
-    await client.query(
-      `UPDATE users SET last_active_at = NOW() WHERE id = '${userId}'`
-    );
-    await client.query("COMMIT");
-    return result.rows[0];
-  } catch (err) {
-    console.log(`could not create message ${err}`);
-    await client.query("ROLLBACK");
-  }
+	try {
+		await client.query("BEGIN");
+		const result = await client.query(
+			"INSERT INTO messages(user_id, text) VALUES($1, $2) RETURNING id, created_date, user_id, text",
+			[userId, text]
+		);
+		await client.query(
+			`UPDATE users SET last_active_at = NOW() WHERE id = '${userId}'`
+		);
+		await client.query("COMMIT");
+		return result.rows[0];
+	} catch (err) {
+		console.log(`could not create message ${err}`);
+		await client.query("ROLLBACK");
+	}
 };
 
 module.exports = {
-  connectToDb,
-  queryInactiveUsers,
-  queryActiveUsers,
-  queryAllMessages,
-  loginUser,
-  logOutUser,
-  putLogoutUser,
-  createMessage,
-  createUser
+	connectToDb,
+	queryInactiveUsers,
+	queryActiveUsers,
+	queryAllMessages,
+	loginUser,
+	logOutUser,
+	putLogoutUser,
+	createMessage,
+	createUser,
 };
--- server.js
+++ server.js
@@ -12,7 +12,7 @@ require("dotenv").config();
 dataAccess.connectToDb();
 
 io.on("connection", socket => {
-  socket.emit("new_message", "sockets connected");
+	socket.emit("new_message", "sockets connected");
 });
 
 app.use(express.static(path.join(__dirname, "client/build")));
@@ -21,98 +21,102 @@ app.use(express.static(path.join(__dirname, "client/build")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+if (process.env.NODE_ENV === "production") {
+	// serve static content
+	app.use(express.static(path.join(__dirname, "client/build")));
+}
 // GET messages
 app.get("/api/messages", async (req, res) => {
-  const messages = await dataAccess.queryAllMessages();
-  res.send(messages);
+	const messages = await dataAccess.queryAllMessages();
+	res.send(messages);
 });
 
 //POST messagesy
 app.post("/api/messages", async (req, res) => {
-  let newMessage = {
-    id: req.body.id,
-    username: req.body.username,
-    text: req.body.text
-  };
+	let newMessage = {
+		id: req.body.id,
+		username: req.body.username,
+		text: req.body.text,
+	};
 
-  const newMessageUserId = req.body.id;
-  const newMessageText = req.body.text;
+	const newMessageUserId = req.body.id;
+	const newMessageText = req.body.text;
 
-  let returnedMessage = await dataAccess.createMessage(
-    newMessageUserId,
-    newMessageText
-  );
+	let returnedMessage = await dataAccess.createMessage(
+		newMessageUserId,
+		newMessageText
+	);
 
-  returnedMessage.username = newMessage.username;
+	returnedMessage.username = newMessage.username;
 
-  io.emit("chat_message", JSON.stringify(returnedMessage));
+	io.emit("chat_message", JSON.stringify(returnedMessage));
 });
 
 //GET all users
 app.get("/api/users", async (req, res) => {
-  if (req.query.active === "true") {
-    const users = await dataAccess.queryActiveUsers();
-    res.send(users);
-  } else {
-    const users = await dataAccess.queryInactiveUsers();
-    res.send(users);
-  }
+	if (req.query.active === "true") {
+		const users = await dataAccess.queryActiveUsers();
+		res.send(users);
+	} else {
+		const users = await dataAccess.queryInactiveUsers();
+		res.send(users);
+	}
 });
 
 //CREATE a user
 app.post("/api/users", async (req, res) => {
-  const newUser = req.body.userName;
-  const password = req.body.password;
+	const newUser = req.body.userName;
+	const password = req.body.password;
 
-  const newUserId = await dataAccess.createUser(newUser, password);
+	const newUserId = await dataAccess.createUser(newUser, password);
 
-  !newUserId
-    ? res.status(401).json({ message: "This user already exists" })
-    : res.status(201).send({ newUserId });
+	!newUserId
+		? res.status(401).json({ message: "This user already exists" })
+		: res.status(201).send({ newUserId });
 });
 
 //LOGIN single user
 app.post("/api/login", async (req, res) => {
-  const loginName = req.body.userName;
-  const password = req.body.password;
-  let userMatch = await dataAccess.loginUser(loginName, password);
+	const loginName = req.body.userName;
+	const password = req.body.password;
+	let userMatch = await dataAccess.loginUser(loginName, password);
 
-  !userMatch.isSuccessful
-    ? res.status(401).json({
-        message: userMatch.errorMessage
-      })
-    : (userMatch = userMatch.user);
+	!userMatch.isSuccessful
+		? res.status(401).json({
+				message: userMatch.errorMessage,
+		  })
+		: (userMatch = userMatch.user);
 
-  res.status(201).send({ userMatch });
+	res.status(201).send({ userMatch });
 
-  io.emit("user_online", JSON.stringify(userMatch));
+	io.emit("user_online", JSON.stringify(userMatch));
 });
 
 app.post("/api/logout", async (req, res) => {
-  const id = req.body.id;
-  let logOutName = req.body.userName;
+	const id = req.body.id;
+	let logOutName = req.body.userName;
 
-  const userMatch = await dataAccess.logOutUser(logOutName);
+	const userMatch = await dataAccess.logOutUser(logOutName);
 
-  !userMatch
-    ? res.status(401).json({
-        message: "there was an issue logging out"
-      })
-    : res.status(201).send({ userMatch });
+	!userMatch
+		? res.status(401).json({
+				message: "there was an issue logging out",
+		  })
+		: res.status(201).send({ userMatch });
 
-  io.emit("user_disconnect", JSON.stringify(id));
+	io.emit("user_disconnect", JSON.stringify(id));
 });
 
 app.put("/api/logout/:id", async (req, res) => {
-  const id = req.params.id;
+	const id = req.params.id;
 
-  await dataAccess.putLogoutUser(id);
+	await dataAccess.putLogoutUser(id);
 
-  io.emit("user_disconnect", JSON.stringify(id));
+	io.emit("user_disconnect", JSON.stringify(id));
 });
 
 app.get("/*", async (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+	res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
 
 server.listen(port, () => console.log(`app with sockets listening at ${port}`));
