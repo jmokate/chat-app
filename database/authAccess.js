56d45eb2366ae2d12e27fb06cea173d6f16cec65
jmokate <jmokate@gmail.com>
2020-09-21 11:52:08 -0500

    working on db route calls

--- database/authAccess.js
+++ database/authAccess.js
@@ -4,64 +4,61 @@ const saltRounds = 10;
 const pgAccess = require("./pg-access");
 require("dotenv").config();
 require("../routes/auth-routes");
-//require("pg");
+require("pg");
 const { Pool } = require("pg");
 const pool = new Pool();
-
-// let client = await pgAccess.connectToDb();
 
 // const client = pgAccess.connectToDb();
 
 loginUser = async (name, password) => {
-	async () => {
-		const client = await pool.connect();
-		try {
-			await client.query("BEGIN");
-			const results = await client.query(
-				`SELECT * FROM users WHERE username = '${name}';`
-			);
-			const saltedPassword = results.rows[0].password;
-			const passwordMatch = await bcrypt.compare(password, saltedPassword);
+	const client = await pgAccess.connectToDb();
+	console.log("connected");
+	try {
+		await client.query("BEGIN");
+		const results = await client.query(
+			`SELECT * FROM users WHERE username = '${name}';`
+		);
+		const saltedPassword = results.rows[0].password;
+		const passwordMatch = await bcrypt.compare(password, saltedPassword);
 
-			if (results.rows[0].is_logged_in == true) {
-				return {
-					isSuccessful: false,
-					errorMessage: "This user is already logged in",
-				};
-			} else if (results.rows[0].is_logged_in == false) {
-				return {
-					isSuccessful: true,
-				};
-			}
-
-			if (!passwordMatch) {
-				return {
-					isSuccessful: false,
-					errorMessage: "Incorrect password",
-				};
-			} else {
-				await client.query(
-					`UPDATE users SET last_active_at = NOW() WHERE username = '${name}'`
-				);
-				await client.query(
-					`UPDATE users SET is_logged_in = true WHERE username = '${name}'`
-				);
-				await client.query("COMMIT");
-			}
-			let successfulLogin = {
-				isSuccessful: true,
-				errorMessage: null,
-				user: results.rows[0],
-			};
-			return successfulLogin;
-		} catch (err) {
-			console.log(`User not logged in ${err}`);
+		if (results.rows[0].is_logged_in == true) {
 			return {
 				isSuccessful: false,
-				errorMessage: "Could not locate this username. Please register.",
+				errorMessage: "This user is already logged in",
+			};
+		} else if (results.rows[0].is_logged_in == false) {
+			return {
+				isSuccessful: true,
 			};
 		}
-	};
+
+		if (!passwordMatch) {
+			return {
+				isSuccessful: false,
+				errorMessage: "Incorrect password",
+			};
+		} else {
+			await client.query(
+				`UPDATE users SET last_active_at = NOW() WHERE username = '${name}'`
+			);
+			await client.query(
+				`UPDATE users SET is_logged_in = true WHERE username = '${name}'`
+			);
+			await client.query("COMMIT");
+		}
+		let successfulLogin = {
+			isSuccessful: true,
+			errorMessage: null,
+			user: results.rows[0],
+		};
+		return successfulLogin;
+	} catch (err) {
+		console.log(`User not logged in ${err}`);
+		return {
+			isSuccessful: false,
+			errorMessage: "Could not locate this username. Please register.",
+		};
+	}
 };
 
 logOutUser = async name => {
--- database/usersAccess.js
+++ database/usersAccess.js
@@ -3,66 +3,58 @@ const saltRounds = 10;
 const pgAccess = require("./pg-access");
 require("dotenv").config();
 require("../routes/users-routes");
-const { Pool } = require("pg");
-const pool = new Pool();
 
 createUser = async (username, password) => {
-	async () => {
-		const client = await pool.connect();
-		console.log("connected to create user");
-		try {
-			const hash = await bcrypt.hashSync(password, saltRounds);
-			await client.query("BEGIN");
-			await client.query(
-				"INSERT INTO users(username, password) VALUES($1, $2) RETURNING id",
-				[username, hash]
-			);
-			const results = await client.query(
-				`SELECT * FROM users WHERE username = '${username}';`
-			);
+	const client = await pgAccess.connectToDb();
+	console.log("connected to create user");
+	try {
+		const hash = await bcrypt.hashSync(password, saltRounds);
+		await client.query("BEGIN");
+		await client.query(
+			"INSERT INTO users(username, password) VALUES($1, $2) RETURNING id",
+			[username, hash]
+		);
+		const results = await client.query(
+			`SELECT * FROM users WHERE username = '${username}';`
+		);
 
-			await client.query("COMMIT");
-			return results.rows[0];
-		} catch (err) {
-			console.log(`User not created ${err}`);
-			await client.query("ROLLBACK");
-			return false;
-		}
-	};
+		await client.query("COMMIT");
+		return results.rows[0];
+	} catch (err) {
+		console.log(`User not created ${err}`);
+		await client.query("ROLLBACK");
+		return false;
+	}
 };
 
 queryInactiveUsers = async () => {
-	async () => {
-		const client = await pool.connect();
-		try {
-			const results = await client.query(
-				"SELECT * FROM users WHERE is_logged_in = false;"
-			);
-			return results.rows;
-		} catch (err) {
-			console.log(`Users not queried ${err}`);
-			return [];
-		}
-	};
+	const client = await pgAccess.connectToDb();
+	try {
+		const results = await client.query(
+			"SELECT * FROM users WHERE is_logged_in = false;"
+		);
+		return results.rows;
+	} catch (err) {
+		console.log(`Users not queried ${err}`);
+		return [];
+	}
 };
 
 queryActiveUsers = async () => {
-	async () => {
-		const client = await pool.connect();
-		try {
-			await client.query("SELECT * FROM users WHERE is_logged_in = true;");
-			await client.query(
-				"UPDATE users SET is_logged_in = false WHERE last_active_at < NOW() - INTERVAL '20 minutes';"
-			);
-			const results = await client.query(
-				"SELECT * FROM users WHERE last_active_at > NOW() - INTERVAL '20 minutes' AND is_logged_in = true;"
-			);
-			return results.rows;
-		} catch (err) {
-			console.log(`Active users not queried ${err}`);
-			return [];
-		}
-	};
+	const client = await pgAccess.connectToDb();
+	try {
+		await client.query("SELECT * FROM users WHERE is_logged_in = true;");
+		await client.query(
+			"UPDATE users SET is_logged_in = false WHERE last_active_at < NOW() - INTERVAL '20 minutes';"
+		);
+		const results = await client.query(
+			"SELECT * FROM users WHERE last_active_at > NOW() - INTERVAL '20 minutes' AND is_logged_in = true;"
+		);
+		return results.rows;
+	} catch (err) {
+		console.log(`Active users not queried ${err}`);
+		return [];
+	}
 };
 
 module.exports = { createUser, queryInactiveUsers, queryActiveUsers };
